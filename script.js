7184adf64a23e26ba87c370cac1dec0d5310bdb6
ProAssassinGaming <Sheybaba101@gmail.com>
2024-03-07 14:08:15 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript for background slideshow
+//let slides = document.querySelectorAll('.slide');
+//let currentSlide = 0;
+//
+//function nextSlide() {
+//    slides[currentSlide].classList.remove('fade');
+//    currentSlide = (currentSlide + 1) % slides.length;
+//    slides[currentSlide].classList.add('fade');
+//}
+
+//Change slide every 5 seconds
+//setInterval(nextSlide, 2000);
